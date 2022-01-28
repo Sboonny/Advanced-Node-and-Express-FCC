f9788fda6722833a629c706d09c2a8b7a8271709
Sboonny <88248797+Sboonny@users.noreply.github.com>
2022-01-28 19:00:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,46 +1,65 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const myDB = require("./connection");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
-const passport = require('passport')
-const session = require('express-session')
+const passport = require("passport");
+const session = require("express-session");
 
-const ObjectID = require('mongodb').ObjectID
-new ObjectID(THE_ID)
+const ObjectID = require("mongodb").ObjectID;
+new ObjectID(THE_ID);
 
 const app = express();
-app.set('view engine', 'pug')
+app.set("view engine", "pug");
 
 fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}))
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
+myDB(async (client) => {
+  const myDataBase = await client.db("database").collection("users");
+
+  app.route("/").get((req, res) => {
+    res.render("pug", {
+      title: "Connecting",
+      message: "please login",
+    });
+  });
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport
+    .deserializeUser((id, done) => {
+      myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+        done(null, null);
+      });
+    })
+    .catch((e) => {
+      app.route("/").get((req, res) => {
+        res.render("pug", { title: e, message: "Unable to login" });
+      });
+    });
 });
-
-passport.deserializeUser((id, done) => {
-  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-  //   done(null, null);
-  // });
-});
-
-app.route('/').get((req, res) => {
-  res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log("Listening on port " + PORT);
 });
+// app.route("/").get((req, res) => {
+//   res.render(process.cwd() + "/views/pug/index", {
+//     title: "Hello",
+//     message: "Please login",
+//   });
+// });
